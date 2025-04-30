865a2c1e39c90f53278efcc98dbdf1d81ae4a3a1
Caneesha <sophie.neufahrt@t-online.de>
2025-04-30 12:46:40 +0200

    Create & test function GetComputerChoice

---
+++ js.js
@@ -0,0 +1,21 @@
+let ComputerChoice;
+let HumanChoice;
+
+function getComputerChoice () {
+    ComputerChoice = Math.random();
+
+    if (ComputerChoice <= 0.33) {
+        console.log("Rock");
+    } else if (ComputerChoice > 0.33 && ComputerChoice <= 0.66) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    };
+}
+
+getComputerChoice(); //delete
+
+function getHumanChoice () {
+   HumanChoice = prompt ("What is your choice?");
+}
+getHumanChoice();
