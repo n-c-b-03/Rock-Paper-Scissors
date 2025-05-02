e275724da1d52258cb3715ecd9c6b52ba47f9758
Caneesha <sophie.neufahrt@t-online.de>
2025-05-02 18:42:35 +0200

    Write and test logic for a Draw

--- js.js
+++ js.js
@@ -6,31 +6,35 @@ function getComputerChoice () {
 
 // forming the random number into "Rock", "Paper" or Scissors:
     if (ComputerChoice <= 0.33) {    
-        console.log("Rock");
+        return "Rock";
     } else if (ComputerChoice > 0.33 && ComputerChoice <= 0.66) {
-        console.log("Paper");
+        return "Paper";
     } else {
-        console.log("Scissors");
+        return "Scissors";
     };
 }
 
 function getHumanChoice () {
     let HumanChoice = prompt ("What is your choice?"); //prompt asking user for choice
+    HumanChoice = HumanChoice.trim();
     HumanChoice = HumanChoice.charAt(0).toUpperCase() + HumanChoice.slice(1).toLowerCase(); //capitalize first letter of HumanChoice and lowercase the rest
     return HumanChoice; // return result of the function
  }
 
+ /**
+   logic for which choice wins over the other:
+    */
 function playRound (HumanChoice, ComputerChoice) { 
-// logic for which choice wins over the other:
-    if (ComputerChoice === "Rock" && HumanChoice === "paper" || ComputerChoice === "Paper" && HumanChoice === "scissors" || ComputerChoice === "Scissors" && HumanChoice === "rock") {
+    if (ComputerChoice === "Rock" && HumanChoice === "Paper" || ComputerChoice === "Paper" && HumanChoice === "Scissors" || ComputerChoice === "Scissors" && HumanChoice === "Rock") {
         console.log(`You win! ${HumanChoice} beats ${ComputerChoice}!`);
         humanScore += 1;
-    } else { 
+    } else if (ComputerChoice === "Rock" && HumanChoice === "Rock" || ComputerChoice === "Paper" && HumanChoice === "Paper" || ComputerChoice === "Scissors" && HumanChoice === "Scissors") {
+        console.log(`Draw between ${ComputerChoice} and ${HumanChoice}!`) 
+        } else { 
         console.log(`You lose! ${ComputerChoice} beats ${HumanChoice}!`);
         computerScore += 1;
     }
 }
-
 
 const humanSelection = getHumanChoice();  
 const computerSelection = getComputerChoice();
